cba60eb8bf83cac246f9ccee9f02248ab4972cb1
Administrator <Administrator@DESKTOP-15TFTUR>
2026-04-20 16:31:25 +0530

    commit

---
+++ Test/src/test/java/Day1/postivenegnum.java
@@ -0,0 +1,22 @@
+package Day1;
+import java.util.Scanner;
+public class postivenegnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+	        if (num > 0) {
+	            System.out.println("Positive number");
+	        } else if (num < 0) {
+	            System.out.println("Negative number");
+	        } else {
+	            System.out.println("Zero");
+	        }
+        }
+	}
+
+
