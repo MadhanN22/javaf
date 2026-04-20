ef7bb39847d3cd7971fa866487fd5517d29821bc
Administrator <Administrator@DESKTOP-15TFTUR>
2026-04-20 21:20:57 +0530

    dsf

---
+++ Test/src/test/java/Day5/DateDuration.java
@@ -0,0 +1,21 @@
+package Day5;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class DateDuration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter date (yyyy-mm-dd): ");
+        String input = sc.nextLine();
+        LocalDate givenDate = LocalDate.parse(input);
+        LocalDate currentDate = LocalDate.now();
+        Period diff = Period.between(givenDate, currentDate);
+        System.out.println("Duration:");
+        System.out.println(diff.getYears() + " Years");
+        System.out.println(diff.getMonths() + " Months");
+        System.out.println(diff.getDays() + " Days");
+        sc.close();
+    }
+}
