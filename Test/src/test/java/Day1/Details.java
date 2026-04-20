5e71efd3fcf096dd56e1bc5eeacd83695b425205
Administrator <Administrator@DESKTOP-15TFTUR>
2026-04-20 15:56:08 +0530

    commit

---
+++ Test/src/test/java/Day1/Details.java
@@ -0,0 +1,22 @@
+package Day1;
+import java.util.Scanner;
+
+public class Details {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String f_name=sc.nextLine();
+		String l_name=sc.nextLine();
+		String gender=sc.nextLine();
+		int age=sc.nextInt();
+		double weight=sc.nextDouble();
+		System.out.println("first_name:"+f_name);
+		System.out.println("last_name:"+l_name);
+		System.out.println("gender:"+gender);
+		System.out.println("age:"+age);
+		System.out.println("weight:"+weight);
+	}
+	
+
+}
