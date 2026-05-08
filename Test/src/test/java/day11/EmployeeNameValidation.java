4d86faaf212036fc0d37a27c484fee3484ca0187
Administrator <Administrator@DESKTOP-15TFTUR>
2026-05-08 13:00:28 +0530

    gff

---
+++ Test/src/test/java/day11/EmployeeNameValidation.java
@@ -0,0 +1,29 @@
+package day11;
+
+import java.util.Scanner;
+
+class NameBlankException extends Exception {
+    public NameBlankException(String message) {
+        super(message);
+    }
+}
+
+public class EmployeeNameValidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.nextLine();
+        String lastName = sc.nextLine();
+        try {
+            validateName(firstName, lastName);
+            System.out.println("Valid full name: " + firstName + " " + lastName);
+        } catch (NameBlankException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void validateName(String firstName, String lastName) throws NameBlankException {
+        if (firstName.trim().isEmpty() || lastName.trim().isEmpty()) {
+            throw new NameBlankException("First name or last name cannot be blank");
+        }
+    }
+}
