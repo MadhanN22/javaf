4afc5a6de66e0c55401397232033e33152047994
Administrator <Administrator@DESKTOP-15TFTUR>
2026-05-08 13:03:01 +0530

    jk

---
+++ Test/src/test/java/day15/ProductArrayListSort.java
@@ -0,0 +1,21 @@
+package day15;
+
+
+import java.util.*;
+
+public class ProductArrayListSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        ArrayList<String> products = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            products.add(sc.nextLine());
+        }
+        Collections.sort(products);
+        for (String p : products) {
+            System.out.println(p);
+        }
+    }
+}
+
