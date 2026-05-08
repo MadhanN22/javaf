cb9dbc95bd2cac4357deba09f5b70244abb1682e
Administrator <Administrator@DESKTOP-15TFTUR>
2026-05-08 13:01:20 +0530

    kjk

---
+++ Test/src/test/java/day12/EmployeeSalaryValidation.java
@@ -0,0 +1,28 @@
+package day12;
+
+import java.util.Scanner;
+
+class EmployeeException extends Exception {
+    public EmployeeException(String message) {
+        super(message);
+    }
+}
+
+public class EmployeeSalaryValidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double salary = sc.nextDouble();
+        try {
+            validateSalary(salary);
+            System.out.println("Valid salary: " + salary);
+        } catch (EmployeeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void validateSalary(double salary) throws EmployeeException {
+        if (salary < 10000) {
+            throw new EmployeeException("Salary must not be below 10000");
+        }
+    }
+}
